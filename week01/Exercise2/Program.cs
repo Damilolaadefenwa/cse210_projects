818407d64ead2299ef7797e83b6e7b0f0d864a95
Damilolaadefenwa <ade24039@byui.edu>
2025-03-08 01:08:11 +0100

    added if statement to the existing code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("Please enter your score number to get your grade: ");
+        string userInput = Console.ReadLine();
+        int gradenumber = int.Parse(userInput);
+
+        string letter = ""; 
+
+        if (gradenumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradenumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradenumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradenumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradenumber >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Oops! Best of luck next time.");
+        }
+
+
     }
 }
