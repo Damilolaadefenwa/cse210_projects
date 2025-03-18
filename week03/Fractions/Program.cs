651bc6af1091e1efd91b2c447735d715286c7b82
Damilolaadefenwa <ade24039@byui.edu>
2025-03-18 22:35:47 +0100

    create the fraction class and program code

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,53 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{    static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // 4. verify that you can create fractions using all three of these constructors. 
+        // For example, create an instance for 1/1 (using the first constructor), 
+        // for 6/1 (using the second constructor), 
+        // for 6/7 (using the third constructor).
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+        // Console.WriteLine(f1);  
+        // Console.WriteLine(f2);  
+        // Console.WriteLine(f3);
+
+      // 5. verify that you can call all of these methods and get the correct values, 
+      // using setters to change the values and then getters to retrieve 
+      // these new values and then display them to the console. 
+
+      f1.SetTop(5);
+      Console.WriteLine(f1.GetTop());
+      f1.SetBottom(1);
+      Console.WriteLine(f1.GetBottom());
+
+      f2.SetTop(6);
+      Console.WriteLine(f2.GetTop());
+      f1.SetBottom(1);
+      Console.WriteLine(f2.GetBottom());
+
+      f3.SetTop(7);
+      Console.WriteLine(f3.GetTop());
+      f3.SetBottom(1);
+      Console.WriteLine(f3.GetBottom());
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
 }
---
+++ week03/Fractions/fraction.cs
@@ -0,0 +1,67 @@
+using System;
+
+// 3. Create the Fraction class.
+// It has two fields / member variables: _top and _bottom, which are the numerator and denominator of the fraction.
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // 4. Create a constructor which is a special method that is called when an object is created.
+    // It is used to initialize the object.
+
+    // a. Create the Constructor that has no parameters that initializes the number to 1/1
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    // b. Create the Constructor Constructor that has one parameter for the top and that initializes the denominator to 1. So that if you pass in the number 5, the fraction would be initialized to 5/1.
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // c. Create the Constructor that has two parameters, one for the top and one for the bottom.
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+
+    // 5. Create getters and setters for both the top and the bottom values.
+
+    // Getter for numerator
+    public int GetTop()
+    {
+        return _top;
+    }
+    // Setter for numerator
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    // Getter for Denominator
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    // Setter for Denominator
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+
+    //6. Create methods to return the representations
+
+
+
+
+
+}
+
+
