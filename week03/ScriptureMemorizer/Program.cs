fa55869880a69836347f96c6175895677dcc2726
Damilolaadefenwa <ade24039@byui.edu>
2025-03-22 14:38:57 +0100

    writing the codes

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Console.WriteLine("--------------------------------------------");
+
+        string _reference = "John 3:16";
+        string _text = "";
+
+        Scripture scripture = new Scripture(_reference, _text);
+
+
+        
+
+
+
+
+
+
+
+
+
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -7,13 +7,36 @@ public class Scripture
     private Reference _reference;
     private List<Word> _entries;
 
-    public Scripture()
+    public Scripture(string referenceText, string scriptureText)
     {
-        
+        _reference = new Reference(referenceText, 1, 1, 1); // Replace 1, 1, 1 with appropriate chapter, startVerse, and endVerse values
+        _entries = scriptureText.Split(' ').Select(word => new Word(word, false)).ToList();
     }
 
+    public void GetDisplayText()
+    {
+        Console.Clear();
+        Console.WriteLine(_reference.GetDisplayText());
+        Console.WriteLine(string.Join(" ", _entries.Select(Word => Word.GetDisplayText())));
+    }
 
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+        int wordsToHide = random.Next(1, _entries.Count / 3 + 1); //Hide 1 to 1/3 of the remaining words
 
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int index = random.Next(_entries.Count);
+            _entries[index].Hide();
+        }
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _entries.All(word => word.isHidden());
+    }
 
 
 }
