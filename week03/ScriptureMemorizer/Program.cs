827df1edff57bd6100b8756f57dd44b04d85594d
Damilolaadefenwa <ade24039@byui.edu>
2025-03-22 15:06:25 +0100

    completed the codes

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,20 +9,32 @@ class Program
         Console.WriteLine("--------------------------------------------");
 
         string _reference = "John 3:16";
-        string _text = "";
+        string _text = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life";
 
         Scripture scripture = new Scripture(_reference, _text);
 
+        scripture.GetDisplayText();
 
+        while (true)
+        {
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+            scripture.GetDisplayText();
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("\nAll words hidden. Program ended.");
         
-
-
-
-
-
-
-
-
-
     }
 }
