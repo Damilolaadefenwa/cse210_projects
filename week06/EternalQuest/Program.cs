cdb74b0ef2574cd1af6b58a0e7be79a342b92c14
Damilolaadefenwa <ade24039@byui.edu>
2025-04-13 13:41:45 +0100

    1st commits

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("-----------------------------------------------");
+        GoalManager mygoals = new GoalManager();
+        mygoals.Start();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+        Console.WriteLine("Goodbye! Thank you for playing the EternalQuest Project.");
+        
     }
+    
 }
