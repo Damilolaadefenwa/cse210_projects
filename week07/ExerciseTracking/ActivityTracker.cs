68a0680b95efb28e630112b2d06bfd26a15de1c4
Damilolaadefenwa <ade24039@byui.edu>
2025-04-16 22:01:05 +0100

    added new lines of codes

--- week07/ExerciseTracking/ActivityTracker.cs
+++ week07/ExerciseTracking/ActivityTracker.cs
@@ -6,31 +6,31 @@ using System.Globalization;
 public class ActivityTracker
 {
     // Encapsulation: Private field for the list of activities
-    private List<Activity> activities;
+    private List<Activity> activitiesList;
 
     // Constructor to initialize the list
     public ActivityTracker()
     {
-        activities = new List<Activity>();
+        activitiesList = new List<Activity>();
     }
 
     // Method to add an activity to the list
     public void AddActivity(Activity activity)
     {
-        activities.Add(activity);
+        activitiesList.Add(activity);
         Console.WriteLine("Activity added successfully.");
     }
 
     // Method to display all activities
     public void DisplayActivities()
     {
-        if (activities.Count == 0)
+        if (activitiesList.Count == 0)
         {
             Console.WriteLine("No activities recorded yet.");
             return;
         }
         Console.WriteLine("Activity Log:");
-        foreach (var activity in activities)
+        foreach (var activity in activitiesList)
         {
             Console.WriteLine(activity.GetSummary()); // Polymorphism: Calls the correct GetSummary() method
         }
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Create an instance of ActivityTracker
+        Console.WriteLine("------------------------------------------------------");
+        
+        // Create an instance of the ActivityTracker
+        ActivityTracker mytracker = new ActivityTracker();
+
+        try
+        {
+            // Create activity objects with sample data
+            Activity running = new Running(DateTime.ParseExact("01/01/2024", "dd/MM/yyyy", CultureInfo.InvariantCulture), 30, 3.0);
+            Activity bicycle = new StationaryBicycle(DateTime.ParseExact("02/01/2024", "dd/MM/yyyy", CultureInfo.InvariantCulture), 45, 20.0);
+            Activity swimming = new Swimming(DateTime.ParseExact("03/01/2024", "dd/MM/yyyy", CultureInfo.InvariantCulture), 60, 30);
+
+            // Add activities to the tracker
+            mytracker.AddActivity(running);
+            mytracker.AddActivity(bicycle);
+            mytracker.AddActivity(swimming);
+
+            // Display all activities
+            mytracker.DisplayActivities();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input format. Please enter date in dd/mm/yyyy and numeric values for others.");
+        }
+        
+        Console.WriteLine();
+
     }
 }
